eb43ce6d99830d852f80998e06deef919cdcb6b4
Ken St. Clair <kenstclair@gmail.com>
2017-11-23 12:11:29 -0500

    server running and serving angular. angular devserver not working yet

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// Get dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,25 +7,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+// Get our API routes
+const api = require('./routes/api');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
+
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'moneymatters/src', 'favicon.ico')));
+// Parsers for POST data
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'moneymatters/dist')));
+
+// Set our api routes
+app.use('/api', api);
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
